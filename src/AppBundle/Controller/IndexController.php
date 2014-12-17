abd040b0bb9d59869259c71e2b63172decb40ce8
Sanpi <sanpi@homecomputing.fr>
2014-12-17 17:14:20 +0100

    Use pomm

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,20 +2,26 @@
 
 namespace AppBundle\Controller;
 
+use \PommProject\Foundation\Pomm;
 use \Symfony\Component\HttpFoundation\Response;
 use \Symfony\Component\Templating\EngineInterface;
 
 class IndexController
 {
+    private $pomm;
     private $templating;
 
-    public function __construct(EngineInterface $templating)
+    public function __construct(EngineInterface $templating, Pomm $pomm)
     {
         $this->templating = $templating;
+        $this->pomm = $pomm;
     }
 
     public function indexAction()
     {
+        $result = $this->pomm['default']->getQueryManager()
+            ->query('select 1');
+
         return new Response(
             $this->templating->render(
                 'AppBundle:Index:index.html.twig'
