069dcbc5c980c66acadb5d0666299053c97012e7
Sanpi <sanpi@homecomputing.fr>
2015-04-23 11:50:53 +0200

    Remove AppCache

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,12 +3,6 @@
 use \Symfony\Component\Debug\Debug;
 use \Symfony\Component\HttpFoundation\Request;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
 if (
     isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
@@ -25,6 +19,7 @@ require_once __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
