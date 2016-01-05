7eba19b98f43fbed542927ac5cc7d533a0ae4420
Sanpi <sanpi@homecomputing.fr>
2016-01-05 16:30:14 +0100

    Update symfony to 3.0

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,10 +12,8 @@ if (
     exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }
 
-$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+require_once __DIR__ . '/../app/autoload.php';
 Debug::enable();
-
-require_once __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
