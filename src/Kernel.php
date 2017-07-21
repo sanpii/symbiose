a94af450f51482d6f51d14f2e98cf93fe4b7e336
Sanpi <sanpi@homecomputing.fr>
2017-07-21 11:42:59 +0200

    Code style

--- src/Kernel.php
+++ src/Kernel.php
@@ -15,17 +15,17 @@ final class Kernel extends HttpKernel
 
     public function getCacheDir(): string
     {
-        return dirname(__DIR__).'/var/cache/'.$this->environment;
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir(): string
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerBundles(): iterable
     {
-        $contents = require dirname(__DIR__).'/config/bundles.php';
+        $contents = require dirname(__DIR__) . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->getEnvironment()])) {
                 yield new $class();
@@ -35,23 +35,23 @@ final class Kernel extends HttpKernel
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $confDir = dirname(__DIR__).'/config';
-        $loader->load($confDir.'/packages/*.yaml', 'glob');
-        if (is_dir($confDir.'/packages/'.$this->getEnvironment())) {
-            $loader->load($confDir.'/packages/'.$this->getEnvironment().'/**/*.yaml', 'glob');
+        $confDir = dirname(__DIR__) . '/config';
+        $loader->load($confDir . '/packages/*.yaml', 'glob');
+        if (is_dir($confDir . '/packages/' . $this->getEnvironment())) {
+            $loader->load($confDir . '/packages/' . $this->getEnvironment() . '/**/*.yaml', 'glob');
         }
-        $loader->load($confDir.'/services.yaml', 'glob');
+        $loader->load($confDir . '/services.yaml', 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        $confDir = dirname(__DIR__).'/config';
-        if (is_dir($confDir.'/routes/')) {
-            $routes->import($confDir.'/routes/*.yaml', '/', 'glob');
+        $confDir = dirname(__DIR__) . '/config';
+        if (is_dir($confDir . '/routes/')) {
+            $routes->import($confDir . '/routes/*.yaml', '/', 'glob');
         }
-        if (is_dir($confDir.'/routes/'.$this->getEnvironment())) {
-            $routes->import($confDir.'/routes/'.$this->getEnvironment().'/**/*.yaml', '/', 'glob');
+        if (is_dir($confDir . '/routes/' . $this->getEnvironment())) {
+            $routes->import($confDir . '/routes/' . $this->getEnvironment() . '/**/*.yaml', '/', 'glob');
         }
-        $routes->import($confDir.'/routes.yaml', '/', 'glob');
+        $routes->import($confDir . '/routes.yaml', '/', 'glob');
     }
 }
