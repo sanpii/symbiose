325bb9940d30bed7d2ffed6229c4d3b8dc0729e8
Sanpi <sanpi@homecomputing.fr>
2017-10-05 17:33:47 +0200

    Load .env for behat tests

--- tests/Features/bootstrap/Bootstrap.php
+++ tests/Features/bootstrap/Bootstrap.php
@@ -2,3 +2,9 @@
 declare(strict_types = 1);
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
+
+$dotenv = __DIR__ . '/../.env';
+if (is_file($dotenv) && class_exists(Dotenv::class)) {
+    (new Dotenv)
+        ->load($dotenv);
+}
